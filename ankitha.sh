807ac75972d070520f8bb25b7258252af348cdbe
Adii <adii...@Adiis-MacBook-Air.local>
2026-01-28 22:27:51 +0530

    variables

--- ankitha.sh
+++ ankitha.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-
-echo "hi ankitha how are you"
-echo "i am fine what about you"
-echo "i am also fine"
 echo "jai ganesh"
+person1=raghu
+person2=ankitha
+echo "${person1} : hi ${person2} how are you"
+echo "${person2} : i am fine ${person1} what about you"
+echo "i am also fine ${person2}"
+
 
