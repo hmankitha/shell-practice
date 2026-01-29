84e55116faf07cf59d12a42cd462bfd09591d42b
Adii <adii...@Adiis-MacBook-Air.local>
2026-01-29 15:18:28 +0530

    updated file

---
+++ logs.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+USERID=$(id -u)
+FILE="var/log/shell-scripting"
+LOG_FILE="var/log/shell-scripting/$0.log"
+
+if [ $USERID -ne 0 ]; then
+    echo "try login as a root user"
+    exit 1
+fi
+
+echo "the nginx is installing"
+dnf install nginx -y &>> LOG_LIFE
+
