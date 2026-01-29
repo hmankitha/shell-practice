ff7c0c6aa3b1491be9bfe3fc6b7b2cb0822c0365
Adii <adii...@Adiis-MacBook-Air.local>
2026-01-29 21:14:17 +0530

    learning

---
+++ 04-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+PERSON1=$1
+PERSON2=$2
+
+echo "hi $PERSON2 how are you?"
+echo "i am file $PERSON1,Thanks for asking How are you"
+echo "ya! i am also fine $PERSON2"
+echo "what are you doing now $PERSON1"
+echo "i am taking the devops course $PERSON2"
