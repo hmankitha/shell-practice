c3c2d10d6ee93b045a4c4b569044d49f60236ab6
Adii <adii...@Adiis-MacBook-Air.local>
2026-01-29 14:45:33 +0530

    updated file

--- 01functions.sh
+++ 01functions.sh
@@ -3,17 +3,39 @@
 installing(){
     if [ "$1" -ne 0 ]; then
         echo "try to login as root user"
-    exit 1
+        exit 1
     fi
 
-    echo " "$2" is installing "
+    echo " $2 is installing "
     dnf install "$2"
 
     if [ "$?" -eq 0 ]; then
         echo "the "$2" is installed sucessfully"
     else 
         echo "the "$2" installation was failed"
+        exit 1
     fi    
-    exit 1
+   
 }
-installing 0 nginx
+installing "$(id -u)" nginx
+
+#!/bin/bash
+
+installing() {
+    if [ "$1" -ne 0 ]; then
+        echo "Try to login as root user"
+        exit 1
+    fi
+
+    echo "$2 is installing..."
+    dnf install -y "$2"
+
+    if [ $? -eq 0 ]; then
+        echo "$2 installed successfully"
+    else
+        echo "$2 installation failed"
+        return 1
+    fi
+}
+
+installing "$(id -u)" nginx
