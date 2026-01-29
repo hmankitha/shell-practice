692d1c043ab395b2dda7d9c785aa615a711e2574
Adii <adii...@Adiis-MacBook-Air.local>
2026-01-29 15:34:59 +0530

    updated file

--- logs.sh
+++ logs.sh
@@ -5,7 +5,7 @@ FILE="var/log/shell-scripting"
 LOG_FILE="var/log/shell-scripting/$0.log"
 
 if [ $USERID -ne 0 ]; then
-    echo "try login as a root user"
+    echo "try login as a root user" | tee -p LOG_FILE
     exit 1
 fi
 
