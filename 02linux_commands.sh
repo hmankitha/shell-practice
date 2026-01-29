c3a41421afa159cd7aee2b18fdcbe8e1f2ac56d0
Adii <adii...@Adiis-MacBook-Air.local>
2026-01-29 12:28:12 +0530

    the linux commands

--- 02linux_commands.sh
+++ 02linux_commands.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
+
 USERID=$(id -u)
-if [(${USERID} ne 0)]; then
-    echo "try to loogin with the root user"
+
+if [ "$USERID" -ne 0 ]; then
+    echo "Please login as root user"
     exit 1
 fi
 
-echo "nginx is installing"
-dnf install nginx
+echo "Nginx is installing..."
+dnf install -y nginx
 
-if [($? eq 0)]; then 
-    echo "nginx installed sucessfull"
-else 
-    echo "nginx failes to install"
+if [ $? -eq 0 ]; then
+    echo "Nginx installed successfully"
+else
+    echo "Nginx failed to install"
+fi    
