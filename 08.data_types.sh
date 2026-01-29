679f0dfdf9fa03c13406147efc64da12a6b18e61
Adii <adii...@Adiis-MacBook-Air.local>
2026-01-29 22:29:11 +0530

    learning

---
+++ 08.data_types.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+NUM1=100
+NUM2=200
+SUM=(($NUM1+$NUM2))
+echo "the addition is $SUM"
+
+#array
+
+fruits=('apple' 'banana' 'greaps')
+echo "the first fruit is $fruits[0]"
+echo "the second fruit is $fruits[1]"
+echo "the thirs fruit is $fruits[2]"
---
+++ 09.conditions.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+NUM=$!
+
+if [ $NUM gt 20 ]; then
+    echo ' the number is greater than the 20'
+else
+    echo ' the nubmber is less than 20 '
+fi    
+
+#gt greater than 
+#lt less than 
+#ne not equal to
+#eq equal
