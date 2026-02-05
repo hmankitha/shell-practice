c28357cc32edef580f75fee5f3c9ed27b03d8d2d
Adii <adii...@Adiis-MacBook-Air.local>
2026-02-05 12:11:34 +0530

    learning

--- 25-backup.sh
+++ 25-backup.sh
@@ -6,6 +6,9 @@ R="\e[31m"
 G="\e[32m" 
 Y="\e[33m" 
 B="\e[34m" 
+SOURCE_DIR=$1
+DEST_DIR=$2
+DAYS=${3:-14} # 14 days is the default value
 
 if [ $USERID -nq 0 ]; then
     echo -e "$R try by login as a root user $N "
@@ -15,10 +18,25 @@ fi
 mkdir -p $LOGS_FOLDER
 
 USAGE(){
-    echo -e "$R USAGE:: sudo backup <SOURCE_DIR> <DAYS>[default 14 days] $N"
+    echo -e "$R USAGE:: sudo backup <SOURCE_DIR> <DEST_DIR> <DAYS>[default 14 days] $N"
     exit 1
 }
 
 if [ $# -lt 2 ]; then
     USAGE 
 fi    
+
+if [ ! -d $SOURCE_DIR ]; then 
+    echo -e "$R #SOURCE_DIR does not exit"
+    exit 1
+fi    
+
+if [ ! -d $SOURCE_DIR ]; then 
+    echo "#SOURCE_DIR does not exit"
+    exit 1
+fi  
+
+if [ ! -d $DEST_DIR ]; then 
+    echo "#DEST_DIR does not exit"
+    exit 1
+fi 
